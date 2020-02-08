25f09041be6219c3ba0f05e9a7b1e32e2edbfad9
Chen Xu <chenxu@Chens-iMac.local>
2020-02-08 22:04:26 +0800

    mod

--- 285.inorder-successor-in-bst.py
+++ 285.inorder-successor-in-bst.py
@@ -58,6 +58,8 @@
 
 class Solution:
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+
+        """
         succ = None
         while root:
             if p.val < root.val:
@@ -67,5 +69,27 @@ class Solution:
                 root = root.right
 
         return succ
+        """
+
+        if p.right:
+            p = p.right
+            while p.left:
+                p = p.left
+            return p
+
+        stack, inorder = [], float("-inf")
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+            if inorder == p.val:
+                return root
+
+            inorder = root.val
+            root = root.right
+
+        return None
         
 
